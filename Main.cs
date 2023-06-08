fbe164f3d612d6761d27d0ff95e7a870251f29dc
Del <delllwo02@gmail.com>
2023-06-09 00:36:04 +0800

    add for (perulangan)

--- Main.cs
+++ Main.cs
@@ -12,6 +12,7 @@ namespace Belajar_Ngoding
         // Method Main atau Entry Point -- Digunakan untuk menjalankan semua program
         public static void Main(string[] args)
         {
+            /*
             Console.WriteLine("-----------------------------------------------------------------------------------");
             Console.WriteLine("Hallo, dah siap belajar ngoding gak?👀");
             Console.WriteLine("Ini adalah method main yang berada di Main.cs, yang akan menjalankan semua program");
@@ -61,9 +62,54 @@ namespace Belajar_Ngoding
                 Console.WriteLine("Yee masih kecill so batona");
             }
 
+            */
             // Perulangan
+            int rows = 5;
             
+            // segitiga siku-siku
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
 
+            // segitiga piramida
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            /*
+            Console.WriteLine("Masukkan jumlah joke yang ingin ditampilkan : ");
+            int jumlahJoke = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < jumlahJoke; i++)
+            {
+                Console.WriteLine("Joke " + (i+1) + ":");
+                Console.WriteLine("Knock Knock! ");
+
+                Console.Write("Who's there? ");
+                string response = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine(response + " who?");
+                string punchline = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine("-------------------------------");
+            }
+            */
         }
     }
 }
